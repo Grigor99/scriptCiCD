b1e02e5dd06cd451b35c4e1adf34ec12a1203558
gmartirosyan <grigor.martirosyan@jambit.com>
2023-04-12 16:00:13 +0400

    script to copy and export into aws ec2 and run the project.

--- src/main/java/com/mycicd/deploy/controller/HelloController.java
+++ src/main/java/com/mycicd/deploy/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "yeah oosqeeeeoooo it's working ,Hellooooo.";
+        return "yeah oosqeeeddeoooo it's working ,Hellooooo.";
     }
 }
