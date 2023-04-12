a5acfa2a0af0d910879ee76b69c78c6136cc8db7
gmartirosyan <grigor.martirosyan@jambit.com>
2023-04-12 20:30:54 +0400

    chnaging and formating script file

--- src/main/java/com/mycicd/deploy/DeployApplication.java
+++ src/main/java/com/mycicd/deploy/DeployApplication.java
@@ -21,7 +21,7 @@ public class DeployApplication {
     @Service
     public class GithubPushChecker {
 
-        @Scheduled(cron = "0 * * * * ?")
+        @Scheduled(cron = "*/1 * * * *")
         public void checkForNewPushes() {
             GitHubClient client = new GitHubClient();
             client.setCredentials("Grigor99", "MyGithub99Ame12345!!"); // replace with your GitHub username and password
