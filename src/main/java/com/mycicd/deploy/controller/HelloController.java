1ff1177e39c7533975acd27b9d826969dceddf6a
gmartirosyan <grigor.martirosyan@jambit.com>
2023-04-12 16:05:53 +0400

    PROJ rfrfrf

--- src/main/java/com/mycicd/deploy/controller/HelloController.java
+++ src/main/java/com/mycicd/deploy/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "yeah oosqeeeddeoooo it's working ,Hellooooo.";
+        return "yeah oosffqeeeddeoooo it's working ,Hellooooo.";
     }
 }
