fef1c6f544ef6608fd51cfbf09cb98824f261cbe
gmartirosyan <grigor.martirosyan@jambit.com>
2023-04-12 20:52:52 +0400

    chnaging and formating script file

--- src/main/java/com/mycicd/deploy/DeployApplication.java
+++ src/main/java/com/mycicd/deploy/DeployApplication.java
@@ -1,5 +1,6 @@
 package com.mycicd.deploy;
 
+import org.apache.tomcat.jni.Proc;
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.RepositoryCommit;
 import org.eclipse.egit.github.core.client.GitHubClient;
@@ -11,7 +12,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Date;
 import java.util.List;
 
@@ -34,14 +38,27 @@ public class DeployApplication {
 
                 Date pushedAt = repository.getPushedAt();
                 if ((System.currentTimeMillis() - pushedAt.getTime()) / 60000D < 1.5) {
-                    Runtime.getRuntime().exec("/Users/gmartirosyan/Downloads/deploy/script.sh");
+                    Process process = Runtime.getRuntime().exec("/Users/gmartirosyan/Downloads/deploy/script.sh");
 
+                    InputStream inputStream = process.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        System.out.println(line); // print the output to the console
+                    }
+
+                    int exitCode = process.waitFor();
+                    System.out.println("Script exited with code " + exitCode);
                 }
                 // check if there is a new commit since the last time the job was run
 
                 // if a new commit is detected, execute the script using the Runtime.exec() method
             } catch (IOException e) {
                 // handle exceptions
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
